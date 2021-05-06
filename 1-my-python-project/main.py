d13c95d6393a1abea05f8bff7f74f4c5325d4816
Rawand Dargali <rdargali@yahoo.com>
2021-05-06 01:59:01 -0500

    multiple parameters

--- 1-my-python-project/main.py
+++ 1-my-python-project/main.py
@@ -1,14 +1,20 @@
-calc_to_seconds = 24*60*60
-name_of_unit = "seconds"
+calc_to_units = 24
+name_of_unit = "hours"
 
-print(f"30 days are {30 * calc_to_seconds} {name_of_unit}")
-# print(f"35 days are {35 * calc_to_seconds} {name_of_unit}")
-# print(f"80 days are {80 * calc_to_seconds} {name_of_unit}")
-# print(f"120 days are {120 * calc_to_seconds} {name_of_unit}")
-
-
-def days_to_units(number_of_days):
-    print(f"{number_of_days} days are {number_of_days * calc_to_seconds} {name_of_unit}")
+# print(f"30 days are {30 * calc_to_units} {name_of_unit}")
+# print(f"35 days are {35 * calc_to_units} {name_of_unit}")
+# print(f"80 days are {80 * calc_to_units} {name_of_unit}")
+# print(f"120 days are {120 * calc_to_units} {name_of_unit}")
 
 
-days_to_units(30)
+def days_to_units(num_of_days, custom_message):
+    print(f"{num_of_days} days are {num_of_days * calc_to_units} {name_of_unit}")
+    print(custom_message)
+    print("----------------------")
+
+
+days_to_units(30, "that's awesome")
+
+days_to_units(50, "no way")
+
+days_to_units(80, "kowabunga")
