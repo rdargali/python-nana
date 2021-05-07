d9eca8071f2373de3c4d7613b8d76d04e37b7856
Rawand Dargali <rdargali@yahoo.com>
2021-05-07 03:52:17 -0500

    clean up and comment out scope check function

--- 1-my-python-project/main.py
+++ 1-my-python-project/main.py
@@ -1,11 +1,6 @@
 calc_to_units = 24
 name_of_unit = "hours"
 
-# print(f"30 days are {30 * calc_to_units} {name_of_unit}")
-# print(f"35 days are {35 * calc_to_units} {name_of_unit}")
-# print(f"80 days are {80 * calc_to_units} {name_of_unit}")
-# print(f"120 days are {120 * calc_to_units} {name_of_unit}")
-
 
 def days_to_units(num_of_days):
     print(num_of_days > 0)
@@ -16,27 +11,19 @@ def days_to_units(num_of_days):
     else:
         return ("incorrect value, please enter value that is greater than zero")
 
-        # my_var = days_to_units(10)
-
-        # print(my_var)
-
-        # days_to_units(30, "that's awesome")
-
-        # days_to_units(50, "no way")
-
-        # days_to_units(80, "kowabunga")
-
-        # def scope_check(parameter_internal_var):
-        #     my_var = "my variable"
-        #     print(name_of_unit)  # global variable
-        #     print(parameter_internal_var)  # local variable as parameter
-        #     print(my_var)  # local variable created inside function
-
-        # scope_check("hello")
-
 
 user_input = input(
     "Hey user, enter a number of days and I will convert it to hours!\n"
 )
 
 print(days_to_units(int(user_input)))
+
+
+# ---SCOPE---
+# def scope_check(parameter_internal_var):
+#     my_var = "my variable"
+#     print(name_of_unit)  # global variable
+#     print(parameter_internal_var)  # local variable as parameter
+#     print(my_var)  # local variable created inside function
+
+# scope_check("hello")
