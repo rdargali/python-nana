3cd715787a9dc14b0cffef68b8f390afc3e318e3
Rawand Dargali <rdargali@yahoo.com>
2021-05-11 02:14:23 -0500

    catch errors with try/except

--- 1-my-python-project/main.py
+++ 1-my-python-project/main.py
@@ -3,7 +3,7 @@ name_of_unit = "hours"
 
 
 def days_to_units(num_of_days):
-    print(num_of_days > 0)
+
     if num_of_days > 0:
         return (f"{num_of_days} days are {num_of_days * calc_to_units} {name_of_unit}")
     elif num_of_days == 0:
@@ -13,15 +13,21 @@ def days_to_units(num_of_days):
 
 
 def validate_input():
-    if user_input.isdigit():
-        print(days_to_units(int(user_input)))
-    elif user_input == "exit":
-        print("You have exited the program")
-    else:
-        print(f"{user_input} is not a valid input, please enter a positive integer")
+    try:
+        user_input_number = int(user_input)
+        if user_input_number > 0:
+            calculated_value = days_to_units(user_input_number)
+            print(calculated_value)
+        elif user_input_number == 0:
+            print("You have entered 0, please enter a positive number")
+        else:
+            print(
+                f"{user_input} is not a valid input, please enter a valid number")
+    except ValueError:
+        print("Your input is not a valid number, please don't ruin my program")
 
 
-user_input = " "
+user_input = ""
 while user_input != "exit":
     user_input = input(
         "Hey user, enter a number of days and I will convert it to hours!\n"
